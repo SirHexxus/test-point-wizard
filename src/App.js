a99560f1e2ce3086931500940d0a01bbb9ab113c
SirHexxus <jamesmichaelstacy@gmail.com>
2020-09-30 13:12:36 -0700

    added lightgray bg to card element

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Card } from 'antd';
 function App() {
   return (
     <div className="App">
-      <Card style={{ width: '75vw', border: '1px solid black', margin: 'auto' }}>
+      <Card style={{ width: '75vw', border: '1px solid black', margin: 'auto', backgroundColor: 'lightgray' }}>
         <h1>Test Point Wizard</h1>
         <InputForm />
       </Card>
