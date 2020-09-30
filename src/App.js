1af4260dad79e1be896ee538b6d006be78a7077d
SirHexxus <jamesmichaelstacy@gmail.com>
2020-09-30 07:58:39 -0700

    pushing to github, to publish on Amplify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import InputForm from './inputForm/InputForm';
+import { Card } from 'antd';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Card style={{ width: '75vw', border: '1px solid black', margin: 'auto' }}>
+        <h1>Test Point Wizard</h1>
+        <InputForm />
+      </Card>
     </div>
   );
 }
