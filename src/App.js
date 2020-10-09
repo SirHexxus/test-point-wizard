500dcd40777824fdfe16e08a4a5a04c5dfa24f60
SirHexxus <jamesmichaelstacy@gmail.com>
2020-10-09 01:02:18 -0700

    minor style and layout refactoring

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './App.css';
-import InputForm from './inputForm/InputForm';
+import InputForm from './components/inputForm/InputForm';
 import { Card } from 'antd';
 
 function App() {
-  return (
-    <div className="App">
-      <Card style={{ width: '75vw', border: '1px solid black', margin: 'auto',marginTop: '1rem', backgroundColor: 'lightgray' }}>
-        <h1>Test Point Wizard</h1>
-        <InputForm />
-      </Card>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Card
+				style={{
+					width: '75vw',
+					border: '1px solid black',
+					margin: 'auto',
+					marginTop: '1rem',
+					backgroundColor: 'lightgray',
+				}}
+			>
+				<h1>Test Point Wizard</h1>
+				<InputForm />
+			</Card>
+		</div>
+	);
 }
 
 export default App;
